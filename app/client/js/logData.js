29ce8a44d822485848aaa51b80c18a67618845f3
drgorb <micha.roon@managination.com>
2014-05-05 12:32:59 +0200

    add pens without data to the end
    Signed-off-by: drgorb <micha.roon@managination.com>

--- app/client/js/logData.js
+++ app/client/js/logData.js
@@ -6,9 +6,14 @@ Template.logData.helpers(
 
       pens: function(){
          var pens = [];
-         _(_(_(Events.find({}, {sort: {timeStamp: -1}}).fetch()).pluck("penId")).uniq()).each(function(penId){
+         var penIdList = _(_(Events.find({}, {sort: {timeStamp: -1}}).fetch()).pluck("penId")).uniq();
+         _(penIdList).each(function(penId){
             pens.push(Pens.findOne({penId: penId}));
          });
+
+         Pens.find({penId: {$nin: penIdList}}).forEach(function(pen){
+            pens.push(pen);
+         })
 
          return pens;
       },
